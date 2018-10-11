7719f997b0fc9e559d5e7dd2164f1dfc94ccd65a
Jessica <jnkm0227@gmail.com>
2018-10-11 14:13:56 -0400

    Updating Week 7 Lab

--- week7_lab/main.js
+++ week7_lab/main.js
@@ -1,9 +1,9 @@
 //global variables
 var animal = [new Monkey(), new Dog(), new Fish()];
-var names = ["T", "Ted", "Teddy"];
+var names = ["Ty", "Ted", "Teddy"];
 
 
-//
+//zoo animals
 function Monkey(name, age) {
   this.name = name;
   this.age = age;
@@ -25,11 +25,43 @@ function Fish(name, age) {
   this.image = "fish.jpg";
 }
 
-
+//setting up functions
 function generateRandomIndex(maxIndex) {
 	return Math.floor(Math.random()* maxIndex);
 }
 
 function generateRandomName() {
-	return generateRandomIndex(name.length);
+	var randomIndex = generateRandomIndex(names.length);
+  return names[randomIndex];
 }
+
+function generateRandomAge() {
+  var randomIndex = generateRandomIndex(5);
+  return randomIndex;
+}
+
+function generateRandomAnimal() {
+  var randomIndex = generateRandomIndex(animal.length);
+  var randomAnimal = animal[randomIndex];
+
+  if (randomAnimal instanceof Monkey) {
+    return new Monkey(generateRandomName(), generateRandomAge());
+  }
+  else if (randomAnimal instanceof Dog) {
+    return new Dog(generateRandomName(), generateRandomAge());
+  }
+  else if (randomAnimal instanceof Fish) {
+    return new Fish(generateRandomName(), generateRandomAge());
+  }
+}
+
+//call upon load
+$(document).ready(function() {
+    var animal = generateRandomAnimal();
+    $("#properties").text(animal.name + "  " + animal.age + " years old");
+    $("#images").attr("src", animal.image);
+});
+
+
+
+
