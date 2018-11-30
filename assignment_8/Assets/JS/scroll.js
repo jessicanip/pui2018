67b0be37e3376773daefb6112b473c2655de7f34
Jessica <jnkm0227@gmail.com>
2018-11-30 00:27:24 -0500

    Replacing folder

--- assignment_8/Assets/JS/scroll.js
+++
@@ -1,9 +0,0 @@
-/* PUI-2018 Assignment 8
-Jessica Nip
-Section A */
-
-/* creating 4 reveal speeds*/
-ScrollReveal().reveal('.order1', { delay: 200 });
-ScrollReveal().reveal('.order2', { delay: 500 });
-ScrollReveal().reveal('.order3', { delay: 800 });
-ScrollReveal().reveal('.order4', { delay: 1100 });
